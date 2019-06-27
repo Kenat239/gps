6a600a01b80a9c338962a5a91e4e281dbcab565c
Gustavo Medina <sistemas.sig@hotmail.com>
2019-06-27 17:03:03 -0500

    Antes de modals

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,4 @@
 import { SocketIoConfig } from 'ngx-socket-io';
 
-export const BACKEND = 'http://localhost:5000';
-export const configSockets: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
+export const BACKEND = 'http://192.168.1.82:5000';
+export const configSockets: SocketIoConfig = { url: 'http://192.168.1.82:5000', options: {} };
